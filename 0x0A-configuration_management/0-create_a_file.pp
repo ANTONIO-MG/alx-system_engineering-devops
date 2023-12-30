4cb50b3ad0bd04fc9cbfcebb90ff4f37e6e70b49
Gerson Antonio <gelsonantonio53@outlook.com>
2023-12-30 12:22:11 +0200

    task 0 comnpleted

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
--- puppet configuration file the creates a file on a agent
+# puppet configuration file the creates a file on a agent
 file {'/tmp/school':
-  ensure  => present,
-  content => 'I love Puppet.',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
+  ensure  => present, # ensures the file is presnet or creates one
+  content => 'I love Puppet.', # writes the content to teh file
+  mode    => '0744', # sets the permissions of the file
+  owner   => 'www-data', # sets the owner of the file
+  group   => 'www-data', # sets the group persmission
 }
