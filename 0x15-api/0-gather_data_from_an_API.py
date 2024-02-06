ff38e908d0505e42d836fe4d13d063e98b760076
Gerson Antonio <gelsonantonio53@outlook.com>
2024-02-06 23:07:47 +0200

    created the readme and completed task 1

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Script that, using thisST API, for given employee ID,
+information about his/ TODO list progress.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    fetches the user data and the TODO list for the given user ID.
+    Calculates the number of completed tasks and prints the employee's
+    name and the number of completed tasks.
+    Prints the title of each completed task.
+    """
+
+    users_url = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+    tasks_url = f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}"
+
+    response1 = requests.get(tasks_url)
+    response2 = requests.get(users_url)
+
+    tasks = response1.json()
+    users = response2.json()
+
+    completed_tasks = [task for task in tasks if task['completed']]
+
+    comp = len(completed_tasks)
+    out_of = len(tasks)
+
+    print(f"Employee {users['name']} is done with tasks({comp}/{out_of}):")
+    for task in completed_tasks:
+        if task['completed'] is True:
+            print(task['title'])
---
+++ 0x15-api/README.md
@@ -0,0 +1,15 @@
+In this project, we aim to explore various aspects of programming, focusing on Python scripting. We will learn about APIs, specifically REST APIs, microservices, data formats such as CSV and JSON, and Python coding style following PEP8 guidelines.
+
+0. Gather data from an API
+Write a Python script that retrieves information about an employee's TODO list progress using a REST API. The script should accept an integer as a parameter (employee ID) and display the employee's TODO list progress in a specific format.
+
+1. Export to CSV
+Extend the previous Python script to export data in CSV format. The CSV file should include information about tasks owned by the employee.
+
+2. Export to JSON
+Extend the Python script to export data in JSON format. The JSON file should include information about tasks owned by the employee.
+
+3. Dictionary of list of dictionaries
+Extend the Python script to export data in JSON format containing records of all tasks from all employees.
+
+    
