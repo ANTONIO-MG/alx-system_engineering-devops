f83b0bb660e7a88ab5f75b0dcd50320cf31b89cc
Gerson Antonio <gelsonantonio53@outlook.com>
2024-02-07 16:41:18 +0200

    formatted the output and removed teh headers

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that, using thisST API, for given employee ID,
+Script forfetching data from API for given employee ID,
 information about his/ TODO list progress.
 """
 import csv
@@ -11,33 +11,31 @@ from sys import argv
 if __name__ == "__main__":
     """
     fetches the user data and the TODO list for the given user ID.
-    Calculates the number of completed tasks and prints the employee's
-    name and the number of completed tasks.
-    Prints the title of each completed task.
+    Store the user ID and surname with the tasks and their status
+    Save all of that information on a CSV file
     """
 
     users_url = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
     tasks_url = f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}"
 
-    response1 = requests.get(tasks_url)
-    response2 = requests.get(users_url)
-
-    tasks = response1.json()
-    users = response2.json()
-
+    # Fetch user info
+    tasks_response = requests.get(tasks_url)
+    users_response = requests.get(users_url)
+    # make Json  objects out of responses
+    tasks = tasks_response.json()
+    users = users_response.json()
+    # name the fields for the CSV
     fields = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
 
+    # create the CSV file...
     with open("USER_ID.csv", 'w', newline='') as file:
         csv_writer = csv.DictWriter(file, fieldnames=fields)
-
-        # write the header
-        csv_writer.writeheader()
 
         # write the rows
         for task in tasks:
             csv_writer.writerow({
                 "USER_ID": users['id'],
-                "USERNAME": users['name'],
+                "USERNAME": users['username'],
                 "TASK_COMPLETED_STATUS": task['completed'],
                 "TASK_TITLE": task['title']
             })
