be4079a1de2a2707888eb945aeef501cc561083c
Gerson Antonio <gelsonantonio53@outlook.com>
2024-05-24 00:45:16 +0200

    updated some files

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,38 +1,43 @@
 #!/usr/bin/python3
-"""  a script that queries the Reddit API and prints the titles of the first
-10 hot posts listed for a given subreddit. """
-
+"""Queries the Reddit API and
+prints the titles of the first
+10 hot posts listed for a given
+subreddit.
+"""
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    """  a function that queries the Reddit API and
-    prints the titles of the first
-    10 hot posts listed for a given subreddit. """
-
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-
-    user_agent = {'User-Agent': "Python/requests"}
-
-    try:
-        response = requests.get(url, headers=user_agent,
-                                allow_redirects=False)
-        titles = response.json()['data']['children']
-
-        if response.status_code != 200:
-            return None
-        else:
-            for title in titles:
-                print(title['data']['title'])
-
-    except Exception as e:
-        print("None:", e)
-
-
-if __name__ == "__main__":
+    """Prints the titles of the first
+    10 hot posts listed for a given
+    subreddit.
     """
-    if module is executed  as a script, then print out the number of
-    """
+    # Set the Default URL strings
+    base_url = 'https://www.reddit.com'
+    api_uri = '{base}/r/{subreddit}/hot.json'.format(base=base_url,
+                                                     subreddit=subreddit)
 
-    top_ten(argv[1])
+    # Set an User-Agent
+    user_agent = {'User-Agent': 'Python/requests'}
+
+    # Set the Query Strings to Request
+    payload = {'limit': '10'}
+
+    # Get the Response of the Reddit API
+    res = requests.get(api_uri, headers=user_agent,
+                       params=payload, allow_redirects=False)
+
+    # Checks if the subreddit is invalid
+    if res.status_code in [302, 404]:
+        print('None')
+    else:
+        res_json = res.json()
+
+        if res_json.get('data') and res_json.get('data').get('children'):
+            # Get the 10 hot posts of the subreddit
+            hot_posts = res_json.get('data').get('children')
+
+            # Print each hot post title
+            for post in hot_posts:
+                if post.get('data') and post.get('data').get('title'):
+                    print(post.get('data').get('title'))
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,48 +1,49 @@
 #!/usr/bin/python3
-""" A recursive Script that queries the Reddit API and returns
-a list containing the titles of all hot articles for a given subreddit """
+"""Queries the Reddit API and
+returns a list containing the
+titles of all hot articles for
+a given subreddit.
 
+If no results are found for the
+given subreddit, the function
+should return None.
+"""
 import requests
-from sys import argv
 
 
-def recurse(subreddit, hot_list=None, after=None):
-    """A recursive function that queries the Reddit API and returns a list
-    containing the titles of all hot articles for a given subreddit."""
-
-    if hot_list is None:
-        hot_list = []
-
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-
-    user_agent = {'User-Agent': "Python/requests"}
-
-    try:
-        params = {'after': after} if after else {}
-        response = requests.get(url, headers=user_agent,
-                                params=params, allow_redirects=False)
-        data = response.json()
-
-        if 'data' in data and 'children' in data['data']:
-            for post in data['data']['children']:
-                hot_list.append(post['data']['title'])
-
-            after = data['data']['after']
-            if after is not None:  # recursively call the function with the 'after'
-                return recurse(subreddit, hot_list, after)
-            else:
-                return hot_list
-        else:
-            return None
-
-    except Exception as e:
-        print("None:", e)
-        return None
-
-
-if __name__ == "__main__":
+def recurse(subreddit, hot_list=[], after=''):
+    """Returns a list containing the titles of all
+    hot articles for a given subreddit.
     """
-    if module is executed  as a script, then print out the number of
-    """
+    # Set the Default URL strings
+    base_url = 'https://www.reddit.com'
+    api_uri = '{base}/r/{subreddit}/hot.json'.format(base=base_url,
+                                                     subreddit=subreddit)
 
-    recurse(argv[1])
+    # Set an User-Agent
+    user_agent = {'User-Agent': 'Python/requests'}
+
+    # Set the Query Strings to Request
+    payload = {'after': after, 'limit': '100'}
+
+    # Get the Response of the Reddit API
+    res = requests.get(api_uri, headers=user_agent,
+                       params=payload, allow_redirects=False)
+
+    # Checks if the subreddit is invalid
+    if res.status_code == 200:
+        res = res.json()
+        hot_posts = res.get('data').get('children')
+        after = res.get('data').get('after')
+
+        # Print each hot post title
+        for post in hot_posts:
+            hot_list.append(post.get('data').get('title'))
+
+        # Get the next page of hot posts
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+
+        return hot_list
+
+    return None
